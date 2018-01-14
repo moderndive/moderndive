71dbe9e1a2a6055d1d3301e5191b200e54dd2d96
ismayc <chester.ismay@gmail.com>
2018-01-13 18:38:08 -0800

    Update tests (95.83% coverage)

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,47 +1,60 @@
 context("get_regression")
 
-test_that("`data` is a data frame", {
+test_that("`model` is an `lm` object", {
   vec <- 1:10
-  expect_error(get_regression_table(formula = vec ~ NULL, data = vec))
+  expect_error(get_regression_table(model = vec))
 })
 
-test_that("regression_table inputs are valid", {
+library(dplyr)
+mpg_null <- lm(mpg ~ NULL, data = mtcars)
+mtcars <- mtcars %>% mutate(cyl = as.factor(cyl))
+mpg_cyl <- lm(mpg ~ cyl, data = mtcars)
+
+iris_binary <- iris %>% 
+  mutate(virginica = Species == "virginica")
+species_glm <- glm(virginica ~ Sepal.Width, data = iris_binary)
+
+test_that("function inputs are valid", {
+  # Check `digits`
   expect_error(get_regression_table(
-    formula = mpg ~ NULL,
-    digits = "blah",
-    data = mtcars
+    model = mpg_null,
+    digits = "blah"
   ))
-  expect_error(get_regression_table(formula = ~ mpg, data = mtcars))
+
+  # Check `print`
+  expect_error(get_regression_points(
+    model = mpg_cyl,
+    digits = 5,
+    print = "yes"
+  ))
   
-  expect_error(get_regression_table(formula = disp + drat ~ mpg, data = mtcars))
-  
-  expect_error(get_regression_points(formula = rats ~ mpg, data = mtcars))
-  
-  expect_error(get_regression_table(formula = mpg ~ rats + cats, data = mtcars))
-  
-  expect_error(get_regression_points(formula = mpg ~ rats + drat, data = mtcars))
+  # Check `model`
+  expect_error(get_regression_summaries(
+    model = species_glm
+  ))
   
 })
 
 test_that("README code works", {
-#  expect_silent(get_regression_table(mpg ~ hp, data = mtcars))
-  expect_silent(get_regression_table(
-    mpg ~ hp + wt,
-    data = mtcars,
-    digits = 4,
-    print = TRUE
-  ))
+  # Convert cyl to factor variable
+  mtcars <- mtcars %>% 
+    mutate(cyl = as.factor(cyl))
   
-  expect_silent(mtcars %>% dplyr::mutate(cyl = as.factor(cyl)) %>%
-                  get_regression_points(formula = mpg ~ hp + cyl, 
-                                        data = .))
+  # Regression models
+  mpg_model <- lm(mpg ~ hp, data = mtcars)
+  mpg_mlr_model <- lm(mpg ~ hp + wt, data = mtcars)
+  mpg_mlr_model2 <- lm(mpg ~ hp + cyl, data = mtcars)
   
-  expect_silent(get_regression_summaries(mpg ~ hp, data = mtcars))
+  # Regression tables
+  expect_silent(get_regression_table(model = mpg_model))
+  expect_silent(get_regression_table(mpg_mlr_model, 
+                                     digits = 4, print = TRUE))
   
-  expect_silent(get_regression_summaries(
-    mpg ~ hp,
-    data = mtcars,
-    digits = 5,
-    print = TRUE
-  ))
-})
+  # Regression points. For residual analysis for example
+  expect_silent(get_regression_points(mpg_mlr_model2))
+  
+  # Regression summaries
+  expect_silent(get_regression_summaries(mpg_model))
+  expect_silent(mpg_model %>% 
+                  get_regression_summaries(digits = 5, print = TRUE))
+ })
