96096f5f41a1b2d9a81d1f56a9d6170402d63316
rudeboybert <albert.ys.kim@gmail.com>
2018-03-08 23:03:00 -0500

    spacing

--- R/house_prices.R
+++ R/house_prices.R
@@ -33,9 +33,11 @@
 #' @examples
 #' library(dplyr)
 #' library(ggplot2)
+#' 
 #' # Create variable log of house price
 #' house_prices <- house_prices %>% 
 #'   mutate(log_price = log(price))
+#'   
 #' # Plot histogram of log of house price
 #' ggplot(house_prices, aes(x = log_price)) +
 #'   geom_histogram()
