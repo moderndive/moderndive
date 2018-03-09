27ff0d8d04060c020224e669364009f28db1e8da
rudeboybert <albert.ys.kim@gmail.com>
2018-03-08 22:00:00 -0500

    Added evals data set

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -1,15 +1,38 @@
 library(tidyverse)
 
+# evals data from: https://www.openintro.org/stat/data/?data=evals
+load(url("http://www.openintro.org/stat/data/evals.RData"))
+evals <- evals %>% 
+  as_tibble() %>% 
+  select(-starts_with("bty_m")) %>% 
+  select(-starts_with("bty_f")) %>% 
+  select(-c(cls_perc_eval, cls_credits, cls_profs)) %>% 
+  mutate(ID = 1:n()) %>% 
+  select(ID, score, age, bty_avg, gender, ethnicity, language, rank, starts_with("pic_"), everything())
+devtools::use_data(evals, overwrite = TRUE)
+
+
+
+
+
 # 10 samples of size n=50 from
 # https://github.com/moderndive/moderndive_book/blob/master/images/sampling_bowl.jpeg
 bowl_samples <- read_csv("data-raw/sampling_responses.csv") %>% 
   mutate(n = red + white + green)
 devtools::use_data(bowl_samples, overwrite = TRUE)
 
+
+
+
+
 # Population of 800 pennies from
 # https://www.statcrunch.com/app/index.php?dataid=301596
 pennies <- read_csv("data-raw/population_of_pennies.csv")
 devtools::use_data(pennies, overwrite = TRUE)
+
+
+
+
 
 # Sampling bowl used in class http://www.qualitytng.com/sampling-bowls/
 set.seed(76)
