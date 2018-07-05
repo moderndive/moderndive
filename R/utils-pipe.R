9f69c3895a5b02874b2c3f0a29e8f6b154f20cb5
ismayc <chester.ismay@gmail.com>
2018-07-05 12:45:05 -0700

    Import pipe using usethis::use_pipe()

---
+++ R/utils-pipe.R
@@ -0,0 +1,12 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr]{\%>\%}} for details. This
+#' file was generated using \code{usethis::use_pipe()}.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
