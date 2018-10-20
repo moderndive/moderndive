3a386986ae7d57d3dc6b4539b71b8f9a1e3f90f7
rudeboybert <albert.ys.kim@gmail.com>
2018-10-20 18:16:20 -0400

    started geom_parallel_slopes()

--- scratchpad.R
+++ scratchpad.R
@@ -1,3 +1,81 @@
+# Let's go!! Defining geom_parallel_slopes()
+
+# https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html
+
+suppressPackageStartupMessages(library(tidyverse))
+set.seed(76)
+library(moderndive)
+data("house_prices")
+house_prices <- house_prices %>%
+  mutate(
+    log10_price = log10(price),
+    log10_size = log10(sqft_living)
+  ) %>% 
+  sample_n(100) 
+
+model_price_3_points <-
+  house_prices %>%
+  lm(log10_price ~ log10_size + condition, data = .) %>%
+  get_regression_points()
+
+
+StatParallelSlopes <- ggproto("StatParallelSlopes", Stat,
+                              
+                              compute_group = function(data, scales) {
+                                output <- data %>%
+                                  lm(y ~ x + group, data = .) %>%
+                                  get_regression_points() %>% 
+                                  select(-y) %>% 
+                                  rename(y = y_hat)
+                                # print(output)
+                                output
+                              },
+                              required_aes = c("x", "y")
+)
+GeomParallelLines <- ggproto("GeomParallelLines", GeomLine,
+                             default_aes = aes(colour = "black", fill = NA, size = 0.5, linetype = 1,
+                                               alpha = NA)
+)
+geom_parallel_slopes <- function(mapping = NULL, data = NULL, 
+                                 position = "identity", na.rm = FALSE, show.legend = NA, 
+                                 inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatParallelSlopes, geom = GeomParallelLines, data = data, mapping = mapping,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+ggplot(house_prices, aes(x = log10_size, y = log10_price, col = condition)) +
+  geom_point(alpha = 0.3) +
+  labs(y = "log10 price", x = "log10 square footage", title = "Seattle House Prices") +
+  # geom_line(data = model_price_3_points, aes(y = log10_price_hat), size = 1.5, show.legend = FALSE) +
+  geom_parallel_slopes(size = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 devtools::install_github("moderndive/moderndive")
 library(moderndive)
 library(tidyverse)
