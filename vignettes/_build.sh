7e6bed5051610ae53eba6db4ab8b40078803fa2b
rudeboybert <albert.ys.kim@gmail.com>
2020-01-10 14:09:27 -0500

    Update bash instructions

--- vignettes/_build.sh
+++ vignettes/_build.sh
@@ -1,3 +1,8 @@
+# This bash script creates paper.md necessary for JOSE testing
+# https://whedon.theoj.org/ and submission. Run this in Terminal first to make 
+# this file executable:
+# chmod +x ./_build.sh
+
 Rscript -e 'rmarkdown::render(input = "why-moderndive.Rmd", output_file = "paper")'
 rm paper.html
 rm why-moderndive.md
