a10435b27f2ce5e2af7ce83eb12e515755e43c96
rudeboybert <albert.ys.kim@gmail.com>
2021-06-17 13:35:21 -0400

    Close #113

--- R/datasets.R
+++ R/datasets.R
@@ -1,3 +1,253 @@
+#' A sample of 50 pennies
+#'
+#' A sample of 50 pennies contained in a 50 cent roll from Florence Bank on
+#' Friday February 1, 2019 in downtown Northampton, Massachusetts, USA
+#' \url{https://goo.gl/maps/AF88fpvVfm12}.
+#'
+#' @format A data frame of 50 rows representing 50 sampled pennies and 2 variables
+#' \describe{
+#'   \item{ID}{Variable used to uniquely identify each penny.}
+#'   \item{year}{Year of minting.}
+#' }
+#' @note The original \code{pennies_sample} has been renamed \code{\link{orig_pennies_sample}}
+#' as of \code{moderndive} v0.3.0.
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(pennies_sample, aes(x = year)) +
+#'   geom_histogram(binwidth = 5, boundary = 2000)
+"pennies_sample"
+
+
+
+#' Bootstrap resamples of a sample of 50 pennies
+#'
+#' 35 bootstrap resamples with replacement of sample of 50 pennies contained in
+#' a 50 cent roll from Florence Bank on Friday February 1, 2019 in downtown Northampton,
+#' Massachusetts, USA \url{https://goo.gl/maps/AF88fpvVfm12}. The original sample
+#' of 50 pennies is available in \code{\link{pennies_sample}} .
+#'
+#' @format A data frame of 1750 rows representing 35 students' bootstrap
+#' resamples of size 50 and 3 variables
+#' \describe{
+#'   \item{replicate}{ID variable of replicate/resample number.}
+#'   \item{name}{Name of student}
+#'   \item{year}{Year on resampled penny}
+#' }
+#' @seealso \code{\link{pennies_sample}}
+#' @examples
+#' library(ggplot2)
+#' library(dplyr)
+#' bootstrap_sample_means <- pennies_resamples %>%
+#'   group_by(name) %>%
+#'   summarize(sample_mean = mean(year))
+#'
+#' ggplot(bootstrap_sample_means, aes(x = sample_mean)) +
+#'   geom_histogram(binwidth = 2.5) +
+#'   labs(x = "sample mean year", title = "Bootstrap distribution of sample mean year")
+"pennies_resamples"
+
+
+
+#' A population of 800 pennies sampled in 2011
+#'
+#' A dataset of 800 pennies to be treated as a sampling population. Data on
+#' these pennies were recorded in 2011.
+#'
+#' @format A data frame of 800 rows representing different pennies and 2 variables
+#' \describe{
+#'   \item{year}{Year of minting}
+#'   \item{age_in_2011}{Age in 2011}
+#' }
+#' @source StatCrunch \url{https://www.statcrunch.com/app/index.php?dataid=301596}
+#' @examples
+#' library(dplyr)
+#' library(ggplot2)
+#'
+#' # Take 25 different samples of size n = 50 pennies from population
+#' many_samples <- pennies %>%
+#'   rep_sample_n(size = 50, reps = 25)
+#' many_samples
+#'
+#' # Compute mean year of minting for each sample
+#' sample_means <- many_samples %>%
+#'   group_by(replicate) %>%
+#'   summarize(mean_year = mean(year))
+#'
+#' # Plot sampling distribution
+#' ggplot(sample_means, aes(x = mean_year)) +
+#'   geom_histogram(binwidth = 1, color = "white") +
+#'   labs(
+#'     x = expression(bar(x)), y = "Number of samples",
+#'     title = "Sampling distribution of x_bar based 25 samples of size n = 50"
+#'   )
+"pennies"
+
+
+
+#' A random sample of 40 pennies sampled from the \code{pennies} data frame
+#'
+#' A dataset of 40 pennies to be treated as a random sample with \code{\link{pennies}} acting
+#' as the population. Data on these pennies were recorded in 2011.
+#'
+#' @format A data frame of 40 rows representing 40 randomly sampled pennies from \code{\link{pennies}} and 2 variables
+#' \describe{
+#'   \item{year}{Year of minting}
+#'   \item{age_in_2011}{Age in 2011}
+#' }
+#' @source StatCrunch \url{https://www.statcrunch.com/app/index.php?dataid=301596}
+#' @seealso \code{\link{pennies}}
+#' @examples
+#' library(dplyr)
+#' library(ggplot2)
+#'
+#' # Take 50 different resamples/bootstraps from the original sample
+#' many_bootstraps <- orig_pennies_sample %>%
+#'   rep_sample_n(size = 40, replace = TRUE, reps = 50)
+#' many_bootstraps
+#'
+#' # Compute mean year of minting for each bootstrap sample
+#' bootstrap_means <- many_bootstraps %>%
+#'   group_by(replicate) %>%
+#'   summarize(mean_year = mean(year))
+#'
+#' # Plot sampling distribution
+#' ggplot(bootstrap_means, aes(x = mean_year)) +
+#'   geom_histogram(binwidth = 1, color = "white") +
+#'   labs(
+#'     x = expression(bar(x)), y = "Number of samples",
+#'     title = "Bootstrap distribution of x_bar based 50 resamples of size n = 40"
+#'   )
+"orig_pennies_sample"
+
+
+
+
+
+
+#' A sampling bowl of red and white balls
+#'
+#' A sampling bowl used as the population in a simulated sampling exercise. Also
+#' known as the urn sampling framework \url{https://en.wikipedia.org/wiki/Urn_problem}.
+#'
+#' @format A data frame 2400 rows representing different balls in the bowl, of which
+#' 900 are red and 1500 are white.
+#' \describe{
+#'   \item{ball_ID}{ID variable used to denote all balls. Note this value is not
+#'   marked on the balls themselves}
+#'   \item{color}{color of ball: red or white}
+#' }
+#' @examples
+#' library(dplyr)
+#' library(ggplot2)
+#'
+#' # Take 10 different samples of size n = 50 balls from bowl
+#' bowl_samples_simulated <- bowl %>%
+#'   rep_sample_n(50, reps = 10)
+#'
+#' # Compute 10 different p_hats (prop red) based on 10 different samples of
+#' # size n = 50
+#' p_hats <- bowl_samples_simulated %>%
+#'   group_by(replicate, color) %>%
+#'   summarize(count = n()) %>%
+#'   mutate(proportion = count / 50) %>%
+#'   filter(color == "red")
+#'
+#' # Plot sampling distribution
+#' ggplot(p_hats, aes(x = proportion)) +
+#'   geom_histogram(binwidth = 0.05) +
+#'   labs(
+#'     x = expression(hat(p)), y = "Number of samples",
+#'     title = "Sampling distribution of p_hat based 10 samples of size n = 50"
+#'   )
+"bowl"
+
+
+
+#' Sampling from a bowl of balls
+#'
+#' Counting the number of red balls in 10 samples of size n = 50 balls from
+#' \url{https://github.com/moderndive/moderndive/blob/master/data-raw/sampling_bowl.jpeg}
+#'
+#' @format A data frame 10 rows representing different groups of students'
+#' samples of size n = 50 and 5 variables
+#' \describe{
+#'   \item{group}{Group name}
+#'   \item{red}{Number of red balls sampled}
+#'   \item{white}{Number of white balls sampled}
+#'   \item{green}{Number of green balls sampled}
+#'   \item{n}{Total number of balls samples}
+#' }
+#' @seealso \code{\link{bowl}}
+#' @examples
+#' library(dplyr)
+#' library(ggplot2)
+#'
+#' # Compute proportion red
+#' bowl_samples <- bowl_samples %>%
+#'   mutate(prop_red = red / n)
+#'
+#' # Plot sampling distributions
+#' ggplot(bowl_samples, aes(x = prop_red)) +
+#'   geom_histogram(binwidth = 0.05) +
+#'   labs(
+#'     x = expression(hat(p)), y = "Number of samples",
+#'     title = "Sampling distribution of p_hat based 10 samples of size n = 50"
+#'   )
+"bowl_samples"
+
+
+
+#' Tactile sampling from a tub of balls
+#'
+#' Counting the number of red balls in 33 tactile samples of size n = 50 balls from
+#' \url{https://github.com/moderndive/moderndive/blob/master/data-raw/sampling_bowl.jpeg}
+#'
+#' @format A data frame of 33 rows representing different groups of students'
+#' samples of size n = 50 and 4 variables
+#' \describe{
+#'   \item{group}{Group members}
+#'   \item{replicate}{Replicate number}
+#'   \item{red_balls}{Number of red balls sampled out of 50}
+#'   \item{prop_red}{Proportion red balls out of 50}
+#' }
+#' @seealso \code{\link{bowl}}
+#' @examples
+#' library(ggplot2)
+#'
+#' # Plot sampling distributions
+#' ggplot(tactile_prop_red, aes(x = prop_red)) +
+#'   geom_histogram(binwidth = 0.025) +
+#'   labs(
+#'     x = expression(hat(p)), y = "Number of samples",
+#'     title = "Sampling distribution of p_hat based 33 samples of size n = 50"
+#'   )
+"tactile_prop_red"
+
+
+
+#' Tactile sample of size 50 from a bowl of balls
+#'
+#' A single tactile sample of size n = 50 balls from
+#' \url{https://github.com/moderndive/moderndive/blob/master/data-raw/sampling_bowl.jpeg}
+#'
+#' @format A data frame of 50 rows representing different balls and 1 variable.
+#' \describe{
+#'   \item{color}{Color of ball sampled}
+#' }
+#' @seealso \code{\link{bowl}}
+#' @examples
+#' library(ggplot2)
+#'
+#' # Barplot of distribution of balls in sample
+#' ggplot(bowl_sample_1, aes(x = color)) +
+#'   geom_bar() +
+#'   labs(title = "50 sampled bals from bowl")
+"bowl_sample_1"
+
+
+
+
 #' Bank manager recommendations based on (binary) gender
 #'
 #' Data from a 1970's study on whether gender influences hiring recommendations.
