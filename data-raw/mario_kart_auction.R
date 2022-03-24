92353292af8cdc9c313ee0e98f2201b2ee18d6e5
Grace Hartley <grace.hartley@gmail.com>
2022-03-24 16:25:49 -0400

    piping csv to read_csv

--- data-raw/mario_kart_auction.R
+++ data-raw/mario_kart_auction.R
@@ -1,4 +1,5 @@
 ## code to prepare `mario_kart_auction` dataset goes here
 library(tidyverse)
-mario_kart_auction <- read_csv("https://www.openintro.org/data/csv/mariokart.csv")
+mario_kart_auction <- "https://www.openintro.org/data/csv/mariokart.csv" %>%
+  read_csv()
 usethis::use_data(mario_kart_auction, overwrite = TRUE)
