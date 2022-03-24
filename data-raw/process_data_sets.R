66cc3e9ed9616d128717c3911a12548f226a2360
TessGold <tessgoldmann@aol.com>
2022-03-24 11:40:43 -0400

    added clean_names()

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -279,4 +279,5 @@ usethis::use_data(pennies_resamples, overwrite = TRUE)
 # https://docs.google.com/spreadsheets/d/1cNuj9V-9Xe8fqV3DQRhvsXJhER3zTkO1dSsQ1Q0j96g/edit#gid=1419070688
 avocado_prices <-
   "https://wjhopper.github.io/SDS-201/data/avocados.csv" %>%
-  read_csv() 
+  read_csv() %>%
+  janitor::clean_names()
