7898d5abc7b1423d301df2462e03e12f18b520cf
silasweden <sweden@smith.edu>
2022-03-24 12:38:50 -0400

    attempt #2 adding csv to data-raw/

--- data-raw/mario-kart-auction.R
+++ data-raw/mario-kart-auction.R
@@ -1,3 +1,4 @@
 ## code to prepare `mario-kart-auction` dataset goes here
-
+library(tidyverse)
+`mario-kart-auction` <- read_csv("https://www.openintro.org/data/csv/mariokart.csv")
 usethis::use_data(mario-kart-auction, overwrite = TRUE)
