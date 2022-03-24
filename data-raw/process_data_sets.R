1dd287bb0fa72ef314497671730f38955677108e
Swaha294 <sbhattacharya@smith.edu>
2022-03-24 11:40:19 -0400

    international power lifting data added, file deleted

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -272,3 +272,11 @@ pennies_resamples <-
   select(replicate, everything()) %>%
   unnest(cols = c(data))
 usethis::use_data(pennies_resamples, overwrite = TRUE)
+
+
+
+## Adding international powerlifting data
+ipf_lifts <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-08/ipf_lifts.csv") %>% 
+  janitor::clean_names()
+
+usethis::use_data(ipf_lifts, overwrite = TRUE)
