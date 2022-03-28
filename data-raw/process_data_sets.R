e4db9ef7944d90931d9cf45fb864f328ed0e9de0
Rose Porta <rporta@smith.edu>
2022-03-28 09:29:05 -0400

    update version in description, move link to separate line

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -275,7 +275,8 @@ usethis::use_data(pennies_resamples, overwrite = TRUE)
 
 # Massachussets 2020 vs. 2019 Traffic Data
 ma_traffic_2020_vs_2019 <- 
-  read_csv("data-raw/Massachusetts_2020_vs_2019_Traffic_Data.csv") %>% 
+  "data-raw/Massachusetts_2020_vs_2019_Traffic_Data.csv" %>% 
+  read_csv() %>% 
   clean_names() %>% 
   mutate(functional_class = as_factor(functional_class))
 usethis::use_data(ma_traffic_2020_vs_2019, overwrite = TRUE)
