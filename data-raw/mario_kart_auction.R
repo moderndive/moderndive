84e051fefbda0b9defeec49db584664258ded84d
Grace Hartley <grace.hartley@gmail.com>
2022-03-30 16:27:14 -0400

    delete file where processing data, moved into the processing data_sets

--- data-raw/mario_kart_auction.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `mario_kart_auction` dataset goes here
-library(tidyverse)
-mario_kart_auction <- "data-raw/mariokart.csv" %>%
-  read_csv()
-usethis::use_data(mario_kart_auction, overwrite = TRUE)
